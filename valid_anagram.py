822aab5518476669eae91eba25cf296e5bc55f52
mr_majed7 <majedulislam7789@gmail.com>
2024-11-13 23:29:44 +0600

    3 Arrays

---
+++ valid_anagram.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/valid-anagram/description/
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        count_s = {}
+        count_t = {}
+        for i in range(len(s)):
+            count_s[s[i]] = 1 + count_s.get(s[i], 0)
+            count_t[t[i]] = 1 + count_t.get(t[i], 0)
+        return count_s == count_t
+
+
+"""To not use extra space sort both string and compare
+    returnsorted(s) === sorted(t)"""
