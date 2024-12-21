b5dce98d9313208614d250df2db9497099f87abd
mr_majed7 <majedulislam7789@gmail.com>
2024-12-21 20:58:34 +0600

    1 Binary Search

---
+++ binary_search/find_min_rotated_array.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        res = nums[0]
+
+        while l <= r:
+            if nums[l] < nums[r]:
+                res = min(res, nums[l])
+                break
+
+            m = (l + r) // 2
+
+            res = min(res, nums[m])
+
+            if nums[m] >= nums[l]:
+                l = m + 1
+
+            else:
+                r = m - 1
+        return res
