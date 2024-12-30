e7685d69344ad601a4aed7066c442b83ccf71952
mr_majed7 <majedulislam7789@gmail.com>
2024-12-30 21:05:41 +0600

    1 Linked List

---
+++ linked_list/find_the_duplicate_num.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/find-the-duplicate-number/description/
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return nums[i]
+        return -1
