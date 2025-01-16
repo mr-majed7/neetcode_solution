d37b26d55a1a8c751c9d686d28648fd0f3297370
mr_majed7 <majedulislam7789@gmail.com>
2025-01-16 22:21:48 +0600

    1 Linked List

---
+++ linked_list/ReorderList.java
@@ -0,0 +1,40 @@
+//https://leetcode.com/problems/reorder-list/description/
+
+public class ReorderList {
+    /**
+     * First we will use slow and first pointer to divide the list in two part
+     * After that we will reverse the second part of the list
+     * With the first part and second reverse part merge them accordingly
+     */
+    public void reorderList(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode second = slow.next;
+        ListNode prev = slow.next = null;
+
+        while (second != null) {
+            ListNode temp = second.next;
+            second.next = prev;
+            prev = second;
+            second = temp;
+        }
+
+        ListNode first = head;
+        second = prev;
+
+        while (second != null) {
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+            first.next = second;
+            second.next = temp1;
+            first = temp1;
+            second = temp2;
+        }
+    }
+}
